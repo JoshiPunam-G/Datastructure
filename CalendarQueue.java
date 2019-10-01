3144511549d3b679cfbee82ef026f8f886186425
JoshiPunam-G <54711717+JoshiPunam-G@users.noreply.github.com>
2019-10-01 19:24:39 +0530

    Add files via upload

--- CalendarQueue.java
+++ CalendarQueue.java
@@ -7,6 +7,8 @@
 
 package com.bridgelabz.datastructure;
 
+import com.bridgelabz.datastructure.LinkedListNext;
+
 import Utility.Utill;
 
 import com.bridgelabz.datastructure.CalendarQ;
@@ -14,7 +16,7 @@ import com.bridgelabz.datastructure.CalendarQ;
 
 public class CalendarQueue {
 
-	public static void main(String[] args) {
+	public static <T> void main(String[] args) {
 	
 	System.out.println("Enter month and year");
 	int month=Utill.isInteger();
@@ -26,59 +28,51 @@ public class CalendarQueue {
 	String[] Months= {"0" ,"Jan" , "Feb" , "Mar" ,"Apr" ,"May" ,"Jun" ,"Jul" ,"Aug" ,"Sept" ,"Oct" ,"Nov" , "Dec"};
 		
 	/* array for no. of days in month*/
-	int[] Days= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int[] Days= { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
 	System.out.println(" "+Months[month]+ "" +year);
 		
-	CalendarQ<String> q = new CalendarQ<String>();
-		
-	CalendarQ<String> cal = new CalendarQ<String>();
-		
 		
 		/*Inserting elements into queue*/
-		q.enQueue(" SUN");
+		LinkedListNext<String> list=new LinkedListNext<String>();
 		
-		q.enQueue(" MON");
-		
-		q.enQueue(" TUE");
-		
-		q.enQueue(" WED");
-		
-		q.enQueue(" THU");
-		
-		q.enQueue(" FRI");
-		
-		q.show();
+		list.enQueue("SUN");
+		list.enQueue("MON");
+		list.enQueue("TUE");
+		list.enQueue("WED");
+		list.enQueue("THU");
+		list.enQueue("FRI");
+		list.enQueue("SAT");
+		list.show();
 		
 		System.out.println();
 	
 		
 
 	/* calculated first day of month*/
-	int d = Utill.DayOfWeek(1, month, year);
+	int d = Utill.dayOfWeek(month, year);
 	
 	//System.out.println(d);
 		
 	/* printing first blank spaces */ 
 	for (int i = 0; i < d; i++) {
-		cal.enQueue("      ");
+		list.enQueue("      ");
 	}
 
 	/*inserting dates in calendar*/
 	for (int i = 1; i <= Days[month]; i++)
 	{
 		if (i < 10)
-			cal.enQueue("  " + i);
+			list.enQueue("  " + i);
 		else
-			cal.enQueue(" " + i);
+			list.enQueue(" " + i);
 		 if((d+i)%7==0)
-				cal.enQueue("\n");
+				list.enQueue("\n");
 	}
 	
 	
 	
 		//printing calendar
-		cal.show();
 		
 		
 	}
